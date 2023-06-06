ef96b85498cfe8f076423eab044c99b56db3baec
Anton Hlinisty <anton.hlinisty@gmail.com>
2023-06-06 22:36:05 +0200

    Improve indentation and line breaks

--- src/calc/equation_solver.cpp
+++ src/calc/equation_solver.cpp
@@ -10,15 +10,18 @@ std::unique_ptr<calc::Solution> calc::EquationSolver::find_general_solution(doub
 
     if (discriminant == 0) {
         long double roots_real_part = -b/(2*a);
-        return std::make_unique<calc::CriticallyDampedSolution>(calc::CriticallyDampedSolution{{roots_real_part, 0}, {roots_real_part, 0}});
+        return std::make_unique<calc::CriticallyDampedSolution>(
+            calc::CriticallyDampedSolution{{roots_real_part, 0}, {roots_real_part, 0}});
     } else if (discriminant > 0) {
         long double first_root_real_part = (-b - std::sqrt(discriminant))/(2*a);
         long double second_root_real_part = (-b + std::sqrt(discriminant))/(2*a);
-        return std::make_unique<calc::OverDampedSolution>(calc::OverDampedSolution{{first_root_real_part, 0}, {second_root_real_part, 0}});
+        return std::make_unique<calc::OverDampedSolution>(
+            calc::OverDampedSolution{{first_root_real_part, 0}, {second_root_real_part, 0}});
     } else {
         long double roots_real_part = -b/(2*a);
         long double first_root_imaginary_part = std::sqrt(-discriminant)/(2*a);
         long double second_root_imaginary_part = -first_root_imaginary_part;
-        return std::make_unique<calc::UnderDampedSolution>(calc::UnderDampedSolution{{roots_real_part, first_root_imaginary_part}, {roots_real_part, second_root_imaginary_part}});
+        return std::make_unique<calc::UnderDampedSolution>(
+            calc::UnderDampedSolution{{roots_real_part, first_root_imaginary_part}, {roots_real_part, second_root_imaginary_part}});
     }
 }
